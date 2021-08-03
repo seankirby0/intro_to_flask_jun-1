6d06c887027713837b4e6c2ef9ee140f86573c06
Brian <bstanton773@gmail.com>
2021-08-03 17:05:05 -0500

    Tuesday afternoon

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import RegisterForm
 
 @app.route('/')
 def index():
@@ -7,7 +8,13 @@ def index():
     title = 'Coding Temple Intro to Flask'
     return render_template('index.html', name=name, title=title)
 
-@app.route('/test')
-def test():
-    colors = ['blue', 'green', 'red', 'orange', 'pink', 'black']
-    return render_template('test.html', title='Test Title!', colors=colors)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(username, email, password)
+        
+    return render_template('register.html', title='Register for CT Blog', form=form)
