a27be20ff86fdd8fa46514890e0c76712b3b3459
Brian <bstanton773@gmail.com>
2021-08-04 15:05:23 -0500

    Adding new users to database via form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template
 from app.forms import RegisterForm
+from app.models import User
 
 @app.route('/')
 def index():
@@ -12,9 +13,17 @@ def index():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        # Grab data from our submitted form
         username = form.username.data
         email = form.email.data
         password = form.password.data
         print(username, email, password)
+        # Create new instance of User
+        new_user = User(username, email, password)
+
+        # Add new_user to our database
+        db.session.add(new_user)
+        db.session.commit()
+
         
     return render_template('register.html', title='Register for CT Blog', form=form)
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-# import os
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Windows - C:\Users\bstan\Documents\codingtemple-jun-2021\week6\day2\intro_to_flask_jun
+# Mac - /home/bstanton/Document/codingtemple-jun-2021/week6/day2/intro_to_flask_jun
 
 class Config:
     SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
